a6e43e83bbee86be6758265631f643cbfdf22e4d
Guilherme Dalazen <guilhermebatistellad@gmail.com>
2020-11-19 15:55:51 -0300

    Adicionando novo detector

--- CatRecognition/analiseTreinamento.py
+++ CatRecognition/analiseTreinamento.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 
 car_cascade = cv2.CascadeClassifier("treinamento/cascade.xml")
-img = cv2.imread("analise01.jpg")
+img = cv2.imread("analise02.jpg")
 height, width, c = img.shape
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 objetos = car_cascade.detectMultiScale(gray, 1.2, 5)
@@ -13,10 +13,10 @@ for (x,y,w,h) in objetos:
 cv2.imshow('Analise', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-# opencv_annotation --annotations=positivas.txt --images=positives/
-# opencv_createsamples -info positivas.txt -bg negativas.txt -vec vetor.vec -w 24 -h 24
-# opencv_traincascade -data treinamento -vec vetor.vec -bg negativas.txt -numPos 40 -numNeg 435 -w 24 -h 24 -precalcValBufSize 1024 -precalcIdxBufSize 1024 -numStages 30 -acceptanceRatioBreakValue 1.0e-5
+ 
+# opencv_annotation --annotations=positivas.txt --images=positivas/
+# opencv_createsamples -info positivas.txt -bg negativas.txt -vec vetor.vec -w 24 -h 24 -num 1000 -vec vetor.vec
+# opencv_traincascade -data treinamento -vec vetor.vec -bg negativas.txt -numPos 77 -numNeg 435 -w 24 -h 24 -precalcValBufSize 1024 -precalcIdxBufSize 1024 -numStages 30 -acceptanceRatioBreakValue 1.0e-5
 
 # opencv_createsamples -img train.jpeg -bg bg.txt -info info/info.txt -pngoutput info -maxxangle 0.5 -maxyangle -0.5 -maxzangle 0.5 -num 1950
 # opencv_traincascade -data treinamento -vec info/info.txt -bg bg.txt -numPos 420 -numNeg 435 -w 24 -h 24 -precalcValBufSize 1024 -precalcIdxBufSize 1024 -numStages 30 -acceptanceRatioBreakValue 1.0e-5
