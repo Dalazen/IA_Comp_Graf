aaaf81be417daa2a0e9749b3ebbaaf8ec46294fd
Guilherme Dalazen <guilhermebatistellad@gmail.com>
2020-11-20 02:28:02 -0300

    Criando novo treinamento para Human detection

--- HumanRecognition/analiseTreinamento.py
+++ HumanRecognition/analiseTreinamento.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-car_cascade = cv2.CascadeClassifier("treinamento/cascade.xml")
+car_cascade = cv2.CascadeClassifier("treinamento 2.0/cascade.xml")
 img = cv2.imread("analise01.jpg")
 height, width, c = img.shape
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -14,11 +14,15 @@ cv2.imshow('Analise', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-# opencv_annotation --annotations=positivas.txt --images=positives/
-# opencv_createsamples -info positivas.txt -bg negativas.txt -vec vetor.vec -w 24 -h 24
-# opencv_traincascade -data treinamento -vec vetor.vec -bg negativas.txt -numPos 40 -numNeg 435 -w 24 -h 24 -precalcValBufSize 1024 -precalcIdxBufSize 1024 -numStages 30 -acceptanceRatioBreakValue 1.0e-5
+'''
+# Treinar com diversas imagens
+opencv_annotation --annotations=positivas.txt --images=positivas/
+opencv_createsamples -info positivas.txt -bg negativas.txt -vec vetor.vec -w 24 -h 24
+opencv_traincascade -data treinamento -vec vetor.vec -bg negativas.txt -numPos 78 -numNeg 200 -w 24 -h 24 -precalcValBufSize 1024 -precalcIdxBufSize 1024 -numStages 60 -acceptanceRatioBreakValue 1.0e-6
 
-# opencv_createsamples -img train.jpeg -bg bg.txt -info info/info.txt -pngoutput info -maxxangle 0.5 -maxyangle -0.5 -maxzangle 0.5 -num 1950
-# opencv_traincascade -data treinamento -vec info/info.txt -bg bg.txt -numPos 420 -numNeg 435 -w 24 -h 24 -precalcValBufSize 1024 -precalcIdxBufSize 1024 -numStages 30 -acceptanceRatioBreakValue 1.0e-5
+# Treinar com uma imagem
+opencv_createsamples -img train.jpeg -bg bg.txt -info info/info.txt -pngoutput info -maxxangle 0.5 -maxyangle -0.5 -maxzangle 0.5 -num 1950
+opencv_traincascade -data treinamento -vec info/info.txt -bg bg.txt -numPos 420 -numNeg 435 -w 24 -h 24 -precalcValBufSize 1024 -precalcIdxBufSize 1024 -numStages 30 -acceptanceRatioBreakValue 1.0e-5
 # Ver a saida
-# opencv_createsamples -w 24 -h 24 -vec relogios.vev
+opencv_createsamples -w 24 -h 24 -vec relogios.vec
+'''
